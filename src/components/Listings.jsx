f94b8a022f1b232f1a08c225f6d7a60c2d53703f
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-09 14:09:30 +1000

    changed URL to heroku deployment. Getting cors errors

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Listing from './Listing'
 
 export default class Listings extends React.Component {
-    state = { listings: [] }
 
     componentDidMount() {
-        fetch(`http://localhost:3000/listings`)
+        fetch(`http://fast-peak-00857.herokuapp.com/listings`)
         .then(response => response.json())
         .then(data => this.setState({listings: data}))
+        .catch((err) => console.log(err));
         console.log('here')
     }
 
     render() {
-        const listingsArr = this.state.listings.map((listing, index) => {
+        const listingsArr = this.state?.listings.map((listing, index) => {
             return <Listing
                 key={index} 
                 id={listing.id} 
